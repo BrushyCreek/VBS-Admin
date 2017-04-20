a55deb87a86aa061e044759e8871b53d76ce633d
Allen Hughes <allen@brushycreekchurch.com>
2017-04-20 16:15:08 -0400

    Fix spelling errors. Fix problems in the tests

--- app/models/kid.rb
+++ app/models/kid.rb
@@ -7,8 +7,8 @@ class Kid < ApplicationRecord
   #TODO: we will need to check and make sure that the default option hasn't been left unchanged
   validates :gender, presence: true
   
-  valedates :address, :city, :zipcode, presence: true
-  valedates :state, presence: true, length: { is: 2 }
+  validates :address, :city, :zipcode, presence: true
+  validates :state, presence: true, length: { is: 2 }
   validates :parents_first_name, :parents_last_name, presence: true
   
   VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
--- test/controllers/kids_controller_test.rb
+++ test/controllers/kids_controller_test.rb
@@ -1,9 +1,5 @@
 require 'test_helper'
 
 class KidsControllerTest < ActionDispatch::IntegrationTest
-  test "should get new" do
-    get kids_new_url
-    assert_response :success
-  end
 
 end
