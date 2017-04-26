ac5a343e3e08e6f7735cddf739456860b9e5a30b
Allen Hughes <allen@brushycreekchurch.com>
2017-04-26 11:05:54 -0400

    clean up new kid form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,23 @@
 module ApplicationHelper
+  def shirt_sizes
+    ["Youth Small", "Youth Medium", "Youth Large", "Small", "Medium", "Large", "XL", "XXL"]
+  end
+  
   def find_age(birthdate)
     now = Time.now.utc.to_date
     now.year - birthdate.year - ((now.month > birthdate.month || (now.month == birthdate.month && now.day >= birthdate.day)) ? 0 : 1)
   end
+  
   def pretty_print_date(date)
-    date.strftime("%d/%m/%Y")
+    date.strftime("%m/%d/%Y")
+  end
+
+  def find_shirt_size(size_id)
+    if size_id
+      return shirt_sizes[size_id]
+    else
+      return "N/A"
+    end
   end
   
 end
