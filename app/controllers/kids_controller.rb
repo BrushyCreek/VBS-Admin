bd6b20da318ccac458a896cf727e400f87a767b2
Allen Hughes <allen@brushycreekchurch.com>
2017-04-26 12:41:01 -0400

    Add basic messaging using flash

--- app/controllers/kids_controller.rb
+++ app/controllers/kids_controller.rb
@@ -6,11 +6,13 @@ class KidsController < ApplicationController
     @kid = Kid.new
   end
   def create
-    @kid = Kid.create(params[:kid])
+    @kid = Kid.create(kid_params)
     if @kid.save
-      render "Kid"
+      flash[:success] = "Child was successfully regiesterd"
+      redirect_to kids_path
     else
-      render "new"
+      flash.now[:warning] = "Something went wrong"
+      render 'new'
     end
   end
   def show
@@ -20,5 +22,38 @@ class KidsController < ApplicationController
       format.html
     end
   end
+  private
   
+  def kid_params
+    params.require(:kid).permit(:first_name,
+                                :last_name,
+                                :birthdate,
+                                :last_grade_id,
+                                :gender,
+                                :address,
+                                :city,
+                                :state,
+                                :zipcode,
+                                :parent_first_name,
+                                :parent_last_name,
+                                :parent_email,
+                                :parent_phone,
+                                :church_member,
+                                :invited_by,
+                                :tshirt_size_id,
+                                :allergies,
+                                :allergies_notes,
+                                :midical_issues,
+                                :medical_issues_notes,
+                                :buddy_request,
+                                :notes,
+                                :pickupper_1_name,
+                                :pickupper_1_phone,
+                                :pickupper_2_name,
+                                :pickupper_2_phone,
+                                :pickupper_3_name,
+                                :pickupper_3_phone,
+                                :highlight,
+                                :can_photograph)
+  end
 end
