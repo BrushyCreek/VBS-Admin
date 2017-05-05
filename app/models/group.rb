d3c1243a5de595c0b82e7201c43a1565b9cda244
Allen Hughes <allen@brushycreekchurch.com>
2017-05-05 15:14:19 -0400

    Add sort_by_name and sort_by_grade scopes to group model

--- app/models/group.rb
+++ app/models/group.rb
@@ -2,6 +2,9 @@ class Group < ApplicationRecord
   has_many :kids
   has_many :volunteers, as: :leader
 
+  scope :sort_by_name, -> { order(name: :asc) }
+  scope :sort_by_grade, -> { order(grade_id: :asc) }
+  
   def name_grade
     grades = ["Pre-K", "Pre-K", "4k", "5k", "1st", "2nd", "3rd", "4th", "5th", "6th"]
 
