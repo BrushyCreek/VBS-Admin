e891f1b3298e7d996ae4d0596c0251a2f930cd55
Allen Hughes <allen@brushycreekchurch.com>
2017-05-06 02:35:42 -0400

    Add and setup Devise gem for authintication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, skip: [:registrations],
+             controller: {
+               sessions: 'users/sessions',
+               passwords: 'users/passwords',
+               registrations: 'users/registrations'
+             }
+
+ # seting up admin scope for :users so as to not conflict with the routes from devise
+ scope "/admin" do
+   resources :users do
+   end
+ end
+  
+               
   resources :volunteers do
   end
   
