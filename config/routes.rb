b74934498e85550f1974ed0d6cce2295ef425248
Allen Hughes <allen@brushycreekchurch.com>
2017-05-06 08:24:03 -0400

    Edit routes to only inclued the registrations actions the we want. Removed 'new_registrations_path' link from devise/shared/_links.html.erb

--- config/routes.rb
+++ config/routes.rb
@@ -6,27 +6,43 @@ Rails.application.routes.draw do
                passwords: 'users/passwords',
                registrations: 'users/registrations'
              }
-
- # seting up admin scope for :users so as to not conflict with the routes from devise
- scope "/admin" do
-   resources :users do
-   end
- end
-  
-               
-  resources :volunteers do
-  end
-  
-  resources :groups do
-  end
-  
-  resources :kids do
-    get 'register', on: :new
-    collection do
-      post 'confirm'
+  # Recreates the Devise registrations routes
+  # They act on a singular user (the signed in user)
+  # Add the actions you want in 'only:'
+    resource :users,
+             only: [:edit, :update, :destroy],
+             controller: 'devise/registrations',
+             as: :user_registration do
+      get 'cancel'
     end
-  end
 
-  root 'kids#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+    authenticated :user do   
+      # Seting up admin scope for :users
+      # so as to not conflict with the routes from devise.
+      # These routes should give users with the admin role
+      # the abbility to edit info for all other users.
+      scope "/admin" do
+        resources :users do
+        end
+      end
+      
+      resources :volunteers do
+      end
+    
+      resources :groups do
+      end
+      
+      resources :kids do
+        get 'register', on: :new
+        collection do
+          post 'confirm'
+        end
+      end
+      
+      root to: 'kids#index', as: :authenticated_root
+    end
+  
+
+    root to: redirect('users/sign_in')
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
