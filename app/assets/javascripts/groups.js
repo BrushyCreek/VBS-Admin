d2220f5d2f603be45ce6328914c7f332a61e3173
Allen Hughes <allen@brushycreekchurch.com>
2017-05-25 17:11:44 -0400

    work in progress for drag and drop, need to make js page specific

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -1,9 +1,11 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
 function dragstart_handler(ev) {
     console.log("dragstart");
     ev.dataTransfer.setData("text/html", ev.target.id);
     ev.dataTransfer.effectAllowed = "move";
+    ev.stopPropagation();
 }
 
 function dragover_handler(ev) {
@@ -15,5 +17,14 @@ function drop_handler(ev) {
     ev.preventDefault();
 
     var data = ev.dataTransfer.getData("text/html");
-    console.log("dropped");
+    var droppedItem = document.getElementById(data);
+    console.log("dropped:", droppedItem);
 }
+
+
+document.addEventListener("turbolinks:load", function() {
+    console.log('Im stil here');
+    var bucketOfKids = document.getElementById('kids-div');
+    console.log(bucketOfKids);
+    bucketOfKids.addEventListener("dragstart", dragstart_handler, false);
+});
