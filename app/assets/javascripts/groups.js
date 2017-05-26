9f60ff7451b8f0fc5397343891021f4e4350ba30
Allen Hughes <allen@brushycreekchurch.com>
2017-05-26 16:07:13 -0400

    working on drag and drop. added dragster.js but drop event dows not fire now

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -1,30 +1,73 @@
-// Place all the behaviors and hooks related to the matching controller here.
-// All this logic will automatically be available in application.js.
+// JS file for groups controller
 
+function contains(list, value) {
+    for( var i = 0; i < list.length; ++i ) {
+        if(list[i] === value) return true;
+    }
+    return false;
+}
 function dragstart_handler(ev) {
+    ev.stopPropagation();
     console.log("dragstart");
     ev.dataTransfer.setData("text/html", ev.target.id);
     ev.dataTransfer.effectAllowed = "move";
-    ev.stopPropagation();
+    console.log("Started dragging:", ev.target);
+}
+
+function dragenter_handler(ev) {
+	ev.stopPropagation();
+	console.log("dragenter");
+	ev.target.classList.add("groupcard-dragover", "card-primary");
+	console.log("Dragged Into:", ev.currentTarget);
 }
 
 function dragover_handler(ev) {
-    ev.preventDefault();
-    ev.dataTransfer.dropEffect = "move";
+    if (contains(ev.dataTransfer.types, "text/html")) {
+	ev.stopPropagation();
+	// console.log("dragging over");
+	ev.preventDefault();
+	ev.dataTransfer.dropEffect = "move";
+    }
+}
+	    
+function dragleave_handler(ev) {
+    ev.stopPropagation();
+    ev.target.classList.remove("groupcard-dragover", "card-primary");
+    console.log("dragging left:", ev.currentTarget);
 }
 
 function drop_handler(ev) {
+    console.log("drop");
     ev.preventDefault();
-
     var data = ev.dataTransfer.getData("text/html");
-    var droppedItem = document.getElementById(data);
-    console.log("dropped:", droppedItem);
+    var draggedElement = document.getElementById(data);
+    console.log("Dropped:", draggedElement);
+    console.log("Dropped on:", ev.currentTarget);
+}
+
+function ancesterWithClass(elm, ancesterClass) {
+    // find the nearest ancestor of element with the attribute of attr
 }
 
 
 document.addEventListener("turbolinks:load", function() {
-    console.log('Im stil here');
-    var bucketOfKids = document.getElementById('kids-div');
-    console.log(bucketOfKids);
-    // bucketOfKids.addEventListener("dragstart", dragstart_handler, false); 
+    if (document.body.classList.contains("groups")) { // this first checks that we are on a page belonging to the correct  rails controller.
+	
+	var bucketOfDraggableKids = document.getElementById("kids-div");
+	console.log(bucketOfDraggableKids);
+	bucketOfDraggableKids.addEventListener("dragstart", dragstart_handler, false);
+
+	var bucketOfDroppableGroups = document.querySelectorAll(".groupcard");
+	console.log(bucketOfDroppableGroups);
+	for (let droppableGroup of bucketOfDroppableGroups) {
+	    new Dragster(droppableGroup);
+	    droppableGroup.addEventListener("dragster:enter", dragenter_handler, false);
+	    // droppableGroup.addEventListener("dragover", dragover_handler, false);
+	    droppableGroup.addEventListener("drop", drop_handler, false);
+	    droppableGroup.addEventListener("dragster:leave", dragleave_handler, false);
+	}
+	
+    } // end if statment testing for body class
 });
+    
+
