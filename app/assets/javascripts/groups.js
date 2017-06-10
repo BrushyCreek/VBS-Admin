d91e44ccfe46626237bb03e88a28b7216ac1ae9e
Allen Hughes <allen@brushycreekchurch.com>
2017-06-10 10:59:44 -0400

    moved all page specific js into if statement testing for controller AND action, including function declarations

--- app/assets/javascripts/groups.js
+++ app/assets/javascripts/groups.js
@@ -1,124 +1,127 @@
 // JS file for groups controller
 
-function contains(list, value) {
-    for( var i = 0; i < list.length; ++i ) {
-        if(list[i] === value) return true;
-    }
-    return false;
-}
-
-function dragstart_handler(ev) {
-    ev.stopPropagation();
-    console.log("dragstart");
-    ev.target.classList.add("draged");
-    ev.dataTransfer.setData("text/html", ev.target.id);
-    ev.dataTransfer.effectAllowed = "move";
-    console.log("Started dragging:", ev.target);
-    //TODO: set the drag image so that it is the whole element being dragged,
-    // currently it will be cut off if the whoe element isnt visible
-}
-
-function dragenter_handler(ev) {
-    ev.preventDefault();
-    ev.stopPropagation();
-    console.log("dragenter");
-    ev.currentTarget.classList.add("dropable");
-    console.log("Dragged Into:", ev.currentTarget);
-}
-
-function dragover_handler(ev) {
-    if (ev.dataTransfer.types.includes("text/html")) {
-	ev.stopPropagation();
-	// console.log("dragging over");
-	ev.preventDefault();
-	ev.dataTransfer.dropEffect = "move";
-	//console.log(ev.dataTransfer.dropEffect);
-    }
-}
-	    
-function dragleave_handler(ev) {
-    ev.stopPropagation();
-    ev.currentTarget.classList.remove("dropable");
-    console.log("dragging left:", ev.currentTarget);
-}
-
-function dragend_handler(ev) {
-    ev.preventDefault();
-    ev.stopPropagation();
-    console.log("dragend");
-    ev.target.classList.remove("draged");
-}
-
-function drop_handler(ev) {
-    ev.preventDefault();
-    ev.stopPropagation();
-    ev.currentTarget.classList.remove("dropable");
-    console.log("drop");
-    var data = ev.dataTransfer.getData("text/html");
-    var ElmToAdd = document.getElementById(data);
-    var destGroupElm = ev.currentTarget;
-    
-    if (ElmToAdd.hasAttribute("data-kid-id")) {
-	// send AJAX request to add succsefullt draged kid to group
-	// reload the table adding kid to top of it
-	// we may need to remove the kids card from the bucket here
-	// so that we can wait to make sure the request went through
-	send_kid_group_change(ElmToAdd, switch_group_kids_table, destGroupElm);
-    } else if (ElmToAdd.hasAttribute("data-volunteer-id")) {
-	send_teacher_group_change(ElmToAdd, switch_group_teachers_row, destGroupElm);
-    }
-}
-
-function send_teacher_group_change (teacherToAddElm, doOnSuccess, destGroupElm) {
-    var teacherToAddId = teacherToAddElm.getAttribute("data-volunteer-id");	
-    var destGroupId = destGroupElm.getAttribute("data-group-id");
-    $.ajax({
-	url:"/groups/update_volunteer_assignment",
-	method: "POST",
-	beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
-	data:{"id" : destGroupId, "volunteer_id" : teacherToAddId},
-	dataType: "html",
-	success: function(response) {
-	    doOnSuccess(destGroupElm, response);
-	    teacherToAddElm.closest(".teachercard").remove();
-	}
-    });
-}
-    
-function switch_group_teachers_row(dest, result) {
-    var row = dest.querySelector(".teacherrow");
-    row.innerHTML = "";
-    row.innerHTML = result;
-}
-
-function send_kid_group_change (kidToAddElm, doOnSuccess, destGroupElm) {
-    var kidToAddId = kidToAddElm.getAttribute("data-kid-id");	
-    var destGroupId = destGroupElm.getAttribute("data-group-id");
-    $.ajax({
-	url:"/groups/update_kid_assignment",
-	method: "POST",
-	beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
-	data:{"id" : destGroupId, "kid_id" : kidToAddId},
-	dataType: "html",
-	success: function(response) {
-	    doOnSuccess(destGroupElm, response);
-	    kidToAddElm.closest(".kidcard").remove();
-	}
-    });
-}
-
-function switch_group_kids_table(dest, result) {
-    var table = dest.querySelector("table");
-    table.innerHTML = "";
-    table.innerHTML = result;
-}
-
-function send_alert(text) {
-    alert(text);
-}
-
 document.addEventListener("turbolinks:load", function() {
-    if (document.body.classList.contains("groups")) { // this first checks that we are on a page belonging to the correct  rails controller.
+    if (document.body.classList.contains("groupsindex")) { // this first checks that we are on a page belonging to the correct  rails controller.
+
+	function contains(list, value) {
+	    for( var i = 0; i < list.length; ++i ) {
+		if(list[i] === value) return true;
+	    }
+	    return false;
+	}
+
+	function dragstart_handler(ev) {
+	    ev.stopPropagation();
+	    console.log("dragstart");
+	    ev.target.classList.add("draged");
+	    ev.dataTransfer.setData("text/html", ev.target.id);
+	    ev.dataTransfer.effectAllowed = "move";
+	    console.log("Started dragging:", ev.target);
+	    //TODO: set the drag image so that it is the whole element being dragged,
+	    // currently it will be cut off if the whoe element isnt visible
+	}
+
+	function dragenter_handler(ev) {
+	    ev.preventDefault();
+	    ev.stopPropagation();
+	    console.log("dragenter");
+	    ev.currentTarget.classList.add("dropable");
+	    console.log("Dragged Into:", ev.currentTarget);
+	}
+
+	function dragover_handler(ev) {
+	    if (ev.dataTransfer.types.includes("text/html")) {
+		ev.stopPropagation();
+		// console.log("dragging over");
+		ev.preventDefault();
+		ev.dataTransfer.dropEffect = "move";
+		//console.log(ev.dataTransfer.dropEffect);
+	    }
+	}
+	
+	function dragleave_handler(ev) {
+	    ev.stopPropagation();
+	    ev.currentTarget.classList.remove("dropable");
+	    console.log("dragging left:", ev.currentTarget);
+	}
+
+	function dragend_handler(ev) {
+	    ev.preventDefault();
+	    ev.stopPropagation();
+	    console.log("dragend");
+	    ev.target.classList.remove("draged");
+	}
+
+	function drop_handler(ev) {
+	    ev.preventDefault();
+	    ev.stopPropagation();
+	    ev.currentTarget.classList.remove("dropable");
+	    console.log("droppp");
+	    var data = ev.dataTransfer.getData("text/html");
+	    var ElmToAdd = document.getElementById(data);
+	    var destGroupElm = ev.currentTarget;
+	    console.log(data);
+	    if (ElmToAdd.hasAttribute("data-kid-id")) {
+		console.log(data);
+		// send AJAX request to add succsefullt draged kid to group
+		// reload the table adding kid to top of it
+		// we may need to remove the kids card from the bucket here
+		// so that we can wait to make sure the request went through
+		send_kid_group_change(ElmToAdd, switch_group_kids_table, destGroupElm);
+	    } else if (ElmToAdd.hasAttribute("data-volunteer-id")) {
+		send_teacher_group_change(ElmToAdd, switch_group_teachers_row, destGroupElm);
+	    }
+	}
+
+	function send_teacher_group_change (teacherToAddElm, doOnSuccess, destGroupElm) {
+	    var teacherToAddId = teacherToAddElm.getAttribute("data-volunteer-id");	
+	    var destGroupId = destGroupElm.getAttribute("data-group-id");
+	    $.ajax({
+		url:"/groups/update_volunteer_assignment",
+		method: "POST",
+		beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+		data:{"id" : destGroupId, "volunteer_id" : teacherToAddId},
+		dataType: "html",
+		success: function(response) {
+		    doOnSuccess(destGroupElm, response);
+		    teacherToAddElm.closest(".teachercard").remove();
+		}
+	    });
+	}
+	
+	function switch_group_teachers_row(dest, result) {
+	    var row = dest.querySelector(".teacherrow");
+	    row.innerHTML = "";
+	    row.innerHTML = result;
+	}
+
+	function send_kid_group_change (kidToAddElm, doOnSuccess, destGroupElm) {
+	    var kidToAddId = kidToAddElm.getAttribute("data-kid-id");	
+	    var destGroupId = destGroupElm.getAttribute("data-group-id");
+	    $.ajax({
+		url:"/groups/update_kid_assignment",
+		method: "POST",
+		beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+		data:{"id" : destGroupId, "kid_id" : kidToAddId},
+		dataType: "html",
+		success: function(response) {
+		    doOnSuccess(destGroupElm, response);
+		    kidToAddElm.closest(".kidcard").remove();
+		}
+	    });
+	}
+
+	function switch_group_kids_table(dest, result) {
+	    var table = dest.querySelector("table");
+	    table.innerHTML = "";
+	    table.innerHTML = result;
+	}
+
+	function send_alert(text) {
+	    alert(text);
+	}
+
+
 
 	$.ajaxSetup({
 	    headers: {
@@ -151,5 +154,5 @@ document.addEventListener("turbolinks:load", function() {
 	
     } // end if statment testing for body class
 });
-    
+
 
--- app/assets/javascripts/locations.js
+++ app/assets/javascripts/locations.js
@@ -1,100 +1,102 @@
 // JS file for locations controller
 
-function contains(list, value) {
-    for( var i = 0; i < list.length; ++i ) {
-        if(list[i] === value) return true;
-    }
-    return false;
-}
-
-function dragstart_handler(ev) {
-    ev.stopPropagation();
-    console.log("dragstart");
-    ev.target.classList.add("draged");
-    ev.dataTransfer.setData("text/html", ev.target.id);
-    ev.dataTransfer.effectAllowed = "move";
-    console.log("Started dragging:", ev.target);
-    //TODO: set the drag image so that it is the whole element being dragged,
-    // currently it will be cut off if the whoe element isnt visible
-}
-
-function dragenter_handler(ev) {
-    ev.preventDefault();
-    ev.stopPropagation();
-    console.log("dragenter");
-    ev.currentTarget.classList.add("dropable");
-    console.log("Dragged Into:", ev.currentTarget);
-}
-
-function dragover_handler(ev) {
-    if (ev.dataTransfer.types.includes("text/html")) {
-	ev.stopPropagation();
-	// console.log("dragging over");
-	ev.preventDefault();
-	ev.dataTransfer.dropEffect = "move";
-	//console.log(ev.dataTransfer.dropEffect);
-    }
-}
-	    
-function dragleave_handler(ev) {
-    ev.stopPropagation();
-    ev.currentTarget.classList.remove("dropable");
-    console.log("dragging left:", ev.currentTarget);
-}
-
-function dragend_handler(ev) {
-    ev.preventDefault();
-    ev.stopPropagation();
-    console.log("dragend");
-    ev.target.classList.remove("draged");
-}
-
-function drop_handler(ev) {
-    ev.preventDefault();
-    ev.stopPropagation();
-    ev.currentTarget.classList.remove("dropable");
-    console.log("drop");
-    var data = ev.dataTransfer.getData("text/html");
-    var ElmToAdd = document.getElementById(data);
-    var destLocationElm = ev.currentTarget;
-    
-    if (ElmToAdd.hasAttribute("data-volunteer-id")) {
-	// send AJAX request to add succsefullt draged kid to group
-	// reload the table adding kid to top of it
-	// we may need to remove the kids card from the bucket here
-	// so that we can wait to make sure the request went through
-	send_volunteer_location_change(ElmToAdd, switch_location_leaders_table, destLocationElm);
-    }
-}
-
-function send_volunteer_location_change (leaderToAddElm, doOnSuccess, destLocationElm) {
-    var leaderToAddId = leaderToAddElm.getAttribute("data-volunteer-id");	
-    var destLocationId = destLocationElm.getAttribute("data-location-id");
-    $.ajax({
-	url:"/locations/update_volunteer_assignment",
-	method: "POST",
-	beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
-	data:{"id" : destLocationId, "volunteer_id" : leaderToAddId},
-	dataType: "html",
-	success: function(response) {
-	    doOnSuccess(destLocationElm, response);
-	    leaderToAddElm.closest(".leadercard").remove();
-	}
-    });
-}
-
-function switch_location_leaders_table(dest, result) {
-    var table = dest.querySelector("table");
-    table.innerHTML = "";
-    table.innerHTML = result;
-}
-
-function send_alert(text) {
-    alert(text);
-}
-
 document.addEventListener("turbolinks:load", function() {
-    if (document.body.classList.contains("locations")) { // this first checks that we are on a page belonging to the correct  rails controller.
+    if (document.body.classList.contains("locationsindex")) { // this first checks that we are on a page belonging to the correct  rails controller.
+
+	function contains(list, value) {
+	    for( var i = 0; i < list.length; ++i ) {
+		if(list[i] === value) return true;
+	    }
+	    return false;
+	}
+
+	function dragstart_handler(ev) {
+	    ev.stopPropagation();
+	    console.log("dragstart");
+	    ev.target.classList.add("draged");
+	    ev.dataTransfer.setData("text/html", ev.target.id);
+	    ev.dataTransfer.effectAllowed = "move";
+	    console.log("Started dragging:", ev.target);
+	    //TODO: set the drag image so that it is the whole element being dragged,
+	    // currently it will be cut off if the whoe element isnt visible
+	}
+
+	function dragenter_handler(ev) {
+	    ev.preventDefault();
+	    ev.stopPropagation();
+	    console.log("dragenter");
+	    ev.currentTarget.classList.add("dropable");
+	    console.log("Dragged Into:", ev.currentTarget);
+	}
+
+	function dragover_handler(ev) {
+	    if (ev.dataTransfer.types.includes("text/html")) {
+		ev.stopPropagation();
+		// console.log("dragging over");
+		ev.preventDefault();
+		ev.dataTransfer.dropEffect = "move";
+		//console.log(ev.dataTransfer.dropEffect);
+	    }
+	}
+	
+	function dragleave_handler(ev) {
+	    ev.stopPropagation();
+	    ev.currentTarget.classList.remove("dropable");
+	    console.log("dragging left:", ev.currentTarget);
+	}
+
+	function dragend_handler(ev) {
+	    ev.preventDefault();
+	    ev.stopPropagation();
+	    console.log("dragend");
+	    ev.target.classList.remove("draged");
+	}
+
+	function drop_handler(ev) {
+	    ev.preventDefault();
+	    ev.stopPropagation();
+	    ev.currentTarget.classList.remove("dropable");
+	    console.log("drop");
+	    var data = ev.dataTransfer.getData("text/html");
+	    var ElmToAdd = document.getElementById(data);
+	    var destLocationElm = ev.currentTarget;
+	    
+	    if (ElmToAdd.hasAttribute("data-volunteer-id")) {
+		// send AJAX request to add succsefullt draged kid to group
+		// reload the table adding kid to top of it
+		// we may need to remove the kids card from the bucket here
+		// so that we can wait to make sure the request went through
+		send_volunteer_location_change(ElmToAdd, switch_location_leaders_table, destLocationElm);
+	    }
+	}
+
+	function send_volunteer_location_change (leaderToAddElm, doOnSuccess, destLocationElm) {
+	    var leaderToAddId = leaderToAddElm.getAttribute("data-volunteer-id");	
+	    var destLocationId = destLocationElm.getAttribute("data-location-id");
+	    $.ajax({
+		url:"/locations/update_volunteer_assignment",
+		method: "POST",
+		beforeSend: function(xhr) {xhr.setRequestHeader('X-CSRF-Token', $('meta[name="csrf-token"]').attr('content'))},
+		data:{"id" : destLocationId, "volunteer_id" : leaderToAddId},
+		dataType: "html",
+		success: function(response) {
+		    doOnSuccess(destLocationElm, response);
+		    leaderToAddElm.closest(".leadercard").remove();
+		}
+	    });
+	}
+
+	function switch_location_leaders_table(dest, result) {
+	    var table = dest.querySelector("table");
+	    table.innerHTML = "";
+	    table.innerHTML = result;
+	}
+
+	function send_alert(text) {
+	    alert(text);
+	}
+
+
 
 	$.ajaxSetup({
 	    headers: {
@@ -123,6 +125,6 @@ document.addEventListener("turbolinks:load", function() {
 	
     } // end if statment testing for body class
 });
-    
+
 
 
