20d1304f17503a06c81da5555c8b947795c87bbe
Allen Hughes <allen@brushycreekchurch.com>
2018-05-17 15:24:47 -0400

    use env variable for domain in mailers

--- app/mailers/registration_mailer.rb
+++ app/mailers/registration_mailer.rb
@@ -1,7 +1,8 @@
 class RegistrationMailer < ApplicationMailer
   def welcome_email
     @family = params[:family]
-    mail(to: @family.head.email,
+    mail(from: 'Registration',
+         to: @family.head.email,
          subject: "Thank you for signing up for I'm With The Band")
   end
 end
